f957156f441fc6b4458c08d3451b2fb1b4e43e17
Gayati-stack123 <gayatrichikkodi80@gmail.com>
2025-04-29 08:53:58 +0530

    Save

--- Employee.java
+++ Employee.java
@@ -2,23 +2,26 @@ public class Employee {
     String name;
     int id;
     double salary;
+    String Address;
     
     //constructor
-    public Employee(String name,int id,double salary){
+    public Employee(String name,int id,double salary,String Address){
         this.name=name;
         this.id=id;
         this.salary=salary;
+        this.Address=Address;
     }
     //method to display 
     public void displayInfo(){
         System.out.println("Employee Id:" + id);
         System.out.println("Employee Name:" + name);
         System.out.println("Employee Salary:" + salary + "₹");
+        System.out.println("Employee Address: " + Address);
     }
 
     //main method
     public static void main(String[] args) {
-        Employee emp=new Employee("Gayatri", 101, 50000.00);
+        Employee emp=new Employee("Gayatri", 101, 50000.00,"Dharwad Managlwar Peth");
         emp.displayInfo();
     }
 }
